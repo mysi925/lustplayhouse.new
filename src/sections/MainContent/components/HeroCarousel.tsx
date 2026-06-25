585ab01ccf673ea97351691ba153bbc0652ee7bc
mysi925 <m4yasi@gmail.com>
2026-06-24 22:25:59 -0700

    Update HeroCarousel.tsx

--- src/sections/MainContent/components/HeroCarousel.tsx
+++ src/sections/MainContent/components/HeroCarousel.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CarouselTrack } from "@/public/previews/video1.mp4";
+import { CarouselTrack } from "@/components/CarouselTrack";
 import { CarouselControls } from "@/sections/MainContent/components/CarouselControls";
 
 export const HeroCarousel = () => {
@@ -8,26 +8,26 @@ export const HeroCarousel = () => {
       <div className="relative max-w-[900px] mx-auto px-3">
         <div className="relative overflow-hidden rounded-2xl">
           <CarouselTrack />
-          {/* Sound toggle */}
+
           <CarouselControls
             variant="absolute top-3 right-[calc(18%+6px)] z-[7] flex items-center justify-center w-10 h-10"
             ariaLabel="Toggle sound"
             buttonText="🔇"
           />
         </div>
-        {/* Prev arrow */}
+
         <CarouselControls
           variant="absolute left-1.5 top-1/2 -translate-y-1/2 z-[6] flex items-center justify-center w-11 h-11 text-[1.7rem]"
           ariaLabel="Previous"
           buttonText="‹"
         />
-        {/* Next arrow */}
+
         <CarouselControls
           variant="absolute right-1.5 top-1/2 -translate-y-1/2 z-[6] flex items-center justify-center w-11 h-11 text-[1.7rem]"
           ariaLabel="Next"
           buttonText="›"
         />
-        {/* Dot indicators */}
+
         <CarouselControls variant="indicators" ariaLabel="" buttonText="" />
       </div>
     </div>
