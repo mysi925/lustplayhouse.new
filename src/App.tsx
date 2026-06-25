41ef3ef1690fbfae45418df400c8461292b5c410
mysi925 <m4yasi@gmail.com>
2026-06-24 18:04:44 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import { Background } from "@/components/Background";
 import { MainContent } from "@/sections/MainContent";
 import { CryptoModal } from "@/components/CryptoModal";
+import Checkout from "./Checkout";
 
-export const App = () => {
+export default function App() {
   return (
     <div className="relative min-h-screen overflow-x-hidden bg-[#08080d] text-[#f0e8e0] font-sans antialiased">
       <Background />
+
       <MainContent />
+
+      {/* PAYMENT UI */}
+      <div className="flex justify-center mt-10">
+        <Checkout />
+      </div>
+
       <CryptoModal />
     </div>
   );
-};
+}
